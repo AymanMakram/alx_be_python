ced5c7bb69718516505734a00b111204205e1026
Ayman <aymanmmokhtar@gmail.com>
2024-09-22 14:12:26 +0300

    lower funcution to result input

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-result = input("Choose the operation (+, -, *, /):")
+result = input("Choose the operation (+, -, *, /):").lower()
 match result:
     case "+":
         print("The result is", num1 + num2)
